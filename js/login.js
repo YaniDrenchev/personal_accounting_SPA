75b653b87de57ffd6b2c91b03dc28dc5af06fe86
qdrenchev <jany.drenchev@gmail.com>
2018-06-28 14:34:52 +0300

    validations added

--- js/login.js
+++ js/login.js
@@ -1,9 +1,16 @@
-$(document).ready(function () {    
+$(document).ready(function () {
     $('#continue').on('click', function () {
-        var firstName = $("#firstName").val();
-        var lastName = $("#lastName").val();
-        $("#intro").html("Hello, " + firstName + " " + lastName);
-        $("#intro2").html("Everything is set up for you, " + firstName + " " + lastName);
-        $(".login-page").fadeOut();        
+        if ($("#firstName").val() == "" || $("#lastName").val() == "") {
+            $('.login-form .error').css('display', 'block');
+        } else {
+            var firstName = $("#firstName").val();
+            var lastName = $("#lastName").val();
+            $("#intro").html("Hello, " + firstName + " " + lastName);
+            $("#intro2").html("Everything is set up for you, " + firstName + " " + lastName);
+            $(".login-page").fadeOut();
+        }
+    })
+    $('#firstName,#lastName').on("keypress", function () {
+        $('.login-form .error').css('display', 'none');
     })
 })
--- js/main.js
+++ js/main.js
@@ -1,44 +1,107 @@
-var accountBalance;
+var accountBalance = 1000;
 $(document).ready(function () {
+    // here you enter your salary and the window fades out
     $("#continue2").on("click", function () {
-        $('.header-container').animate({
-            opacity: 0.05,
-            padding: 0.05,
-            left: "+=50",
-            height: "toggle"
-        }, 500)
-        accountBalance = $('#salary').val();
-        $('#balance').html(accountBalance + "BGN");
+        // validation
+        if ($('#salary').val() == "" || $('#salary').val().length < 2) {
+            $('.header-container .error').css('display', 'block');
+        } else {
+            $('.header-container').animate({
+                opacity: 0.05,
+                padding: 0.05,
+                left: "+=50",
+                height: "toggle"
+            }, 500)
+            accountBalance = $('#salary').val();
+            $('#balance').html(accountBalance + "BGN");
+        }
     });
 
     $('#spended').on("click", function () {
-        if ($('#spendedMoney').val() == '') {
-            $('.spendings .error').css('display', '');
+        if ($('#spendedMoney').val() == '' && $('#spended-select').val() == "") {
+            $('.spendings .error').html("Please enter a valid value and reason ")
+                .css('display', 'block');
+        } else if ($('#spended-select').val() == "") {
+            $('.spendings .error').html("Please enter a valid reason ")
+                .css('display', 'block');
+        } else if ($('#spendedMoney').val() == '') {
+            $('.spendings .error').html("Please enter a valid value ")
+                .css('display', 'block');
         } else {
             var spendedMoney = $('#spendedMoney').val();
-            $('#transactions_history').append("<div> - " + spendedMoney + "</div>");
+            $('#transactions_history')
+            .append("<div>" + "You spend " + spendedMoney + 
+            "BGN " + "for " + $('#spended-select').val()+ "</div>");
             accountBalance = parseInt(accountBalance) - parseInt(spendedMoney);
             $('#balance').html(accountBalance + "BGN");
+            if(accountBalance <= 100){
+                $('#modal').css('display', 'block');
+            
+            }
         }
     })
     $('#mothlySpendings-btn').on("click", function () {
-        if ($('#spendedMoney').val() == '') {
-            $('.spendings .error').css('display', '');
+        if ($('#mothlySpendingsVal').val() == '' && $('#monthly-select').val() == "") {
+            $('.mothlySpendings .error').html("Please enter a valid value and reason ")
+                .css('display', 'block');
+        } else if ($('#monthly-select').val() == "") {
+            $('.mothlySpendings .error').html("Please enter a valid reason ")
+                .css('display', 'block');
+        } else if ($('#mothlySpendingsVal').val() == '') {
+            $('.mothlySpendings .error').html("Please enter a valid value ")
+                .css('display', 'block');
         } else {
             var spendedMoney = $('#mothlySpendingsVal').val();
-            $('#transactions_history').append("<div> - " + spendedMoney + "</div>");
+            $('#transactions_history')
+            .append("<div>" + "You spend " + spendedMoney + 
+            "BGN " + "for " + $('#monthly-select').val()+ "</div>");
             accountBalance = parseInt(accountBalance) - parseInt(spendedMoney);
             $('#balance').html(accountBalance + "BGN");
+            if(accountBalance <= 100){
+                $('#modal').css('display', 'block');
+            
+            }
         }
     })
     $('#income-btn').on("click", function () {
-        if ($('#spendedMoney').val() == '') {
-            $('.spendings #error').css('display', '');
-        } else {
+        if ($('#incomeVal').val() == '' && $('#income-select').val() == "") {
+            $('.income .error').html("Please enter a valid value and reason ")
+            .css('display', 'block');
+        }else if ($('#income-select').val() == "") {
+            $('.income .error').html("Please enter a valid reason ")
+            .css('display', 'block');
+        } else if ($('#incomeVal').val() == '') {
+            $('.income .error').html("Please enter a valid value ")
+            .css('display', 'block');
+        }
+        else {
             var spendedMoney = $('#incomeVal').val();
-            $('#transactions_history').append("<div> + " + spendedMoney + "</div>");
+            $('#transactions_history')
+            .append("<div>" + "You earned " + spendedMoney + 
+            "BGN " + "from " + $('#income-select').val()+ "</div>");
             accountBalance = parseInt(accountBalance) + parseInt(spendedMoney);
             $('#balance').html(accountBalance + "BGN");
+            if(accountBalance <= 100){
+                $('#modal').css('display', 'block');
+            
+            }
         }
+
+       
+            
     })
+    //error message reset
+    $('#mothlySpendingsVal').on("keypress", function () {
+        $('.mothlySpendings .error').css('display', 'none');
+    })
+    $('#spendedMoney').on("keypress", function () {
+        $('.spendings .error').css('display', 'none');
+    })
+    $('#incomeVal').on("keypress", function () {
+        $('.income .error').css('display', 'none');
+    })
+    $('#salary').on("keypress", function () {
+        $('.header-container .error').css('display', 'none');
+    })
+
 })
